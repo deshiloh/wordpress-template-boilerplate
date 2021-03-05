9932c563ba70baa437fec8ca6ecfce75d899099a
Alvarez-Iglisias Manuel <m.alvarez.iglisias@gmail.com>
2021-03-05 15:07:00 +0100

    feat(Theme support): ajoute divers theme support essentiel.

--- functions.php
+++ functions.php
@@ -6,3 +6,28 @@ function add_theme_scripts() {
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/dist/js/app.js', [], 1.0, true);
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+/**
+ * Essential theme supports
+ * */
+function theme_setup(){
+    /** automatic feed link*/
+    add_theme_support( 'automatic-feed-links' );
+
+    /** tag-title **/
+    add_theme_support( 'title-tag' );
+
+    /** post formats */
+    $post_formats = array('aside','image','gallery','video','audio','link','quote','status');
+    add_theme_support( 'post-formats', $post_formats);
+
+    /** post thumbnail **/
+    add_theme_support( 'post-thumbnails' );
+
+    /** HTML5 support **/
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+    /** refresh widgest **/
+    add_theme_support( 'customize-selective-refresh-widgets' );
+}
+add_action('after_setup_theme','theme_setup');
