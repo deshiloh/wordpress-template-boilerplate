bbafe3108ee11e418c8c8d59dc76dcaca445aecf
Alvarez-Iglisias Manuel <m.alvarez.iglisias@gmail.com>
2021-03-08 16:31:04 +0100

    feat(Plugins): met en place la dépendence de plugins majoritairement mis en place dans un site de dev.

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__.'/inc/plugins-activation/activations.php';
+
 function add_theme_scripts() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_style( 'main_style', get_template_directory_uri().'/dist/css/app.css');
